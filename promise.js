c5af1080bc0388465409b6df8990b1782692adfa
郭鹏 <guopeng@guopengdeMacBook-Pro.local>
2017-02-13 22:33:41 +0800

    promise示例

--- promise.js
+++ promise.js
@@ -1,38 +1,79 @@
-/*function readFile() {
+function readFile1() {
     return new Promise(function(resolve, reject) {
         setTimeout(function() {
-            var err = Math.random() < 0.5;
-            if (err) {
-                reject('error');
-            }
-            else {
-                resolve('success');
-            }
-        }, 2000);
+            resolve(1);
+        }, 1000);
     });
 }
 
-var promise = readFile();
-promise.then(function(data) {
+function readFile2() {
+    return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            console.log('finished')
+            resolve(2);
+        }, 1000);
+    });
+}
+
+function readFile3() {
+    return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            resolve(3);
+        }, 1000);
+    });
+}
+
+var p4 = Promise.all([readFile1(), readFile2(), readFile3()]);
+p4.then(function(data) {
     console.log(data);
 });
-promise.catch(function(err) {
+
+
+//var promise = readFile();
+//promise.then(function(data) {console.log(data)});
+//promise.catch(function(err) {console.log(err)});
+
+
+
+//promise chains
+//怎么解决传值
+//成功怎么调失败，失败怎么调成功
+
+
+function fetch(url) {
+    return new Promise(function(resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                resolve(JSON.parse(xhr.responseText));
+            }
+        }
+        xhr.onerror = function() {
+            reject('error');
+        }
+        xhr.send(null);
+    });
+}
+
+fetch('./getJson').then(function(data) {
+    console.log(data);
+}).catch(function(err) {
     console.log(err);
 });
-setTimeout(function() {
-    promise.then(function(data) {
-        console.log('2' + data);
-    })
-}, 4000)*/
 
-var promise = Promise.resolve(1);
-promise.then(function(data) {
-    console.log(data);
-});
-console.log(2);
-promise.then(function(data) {
-    console.log(3)
-});
+sleep(10000);
+
+function sleep(m) {
+    var start = Date.now();
+    while((Date.now() - start) < m) {}
+}
+
+/**
+ * promise并不能取代event和callback
+ * promise更适合状态比较少的异步操作
+ * 如果状态比较多的话，event和cb更灵活
+ */
 
 
 
